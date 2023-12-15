2cf28df5993c0bf88dc402132207ab1ed50b63ae
mariaortizz <ortizmariadelrosario@gmail.com>
2023-12-15 18:44:46 +0200

    ejer 16/12

--- src/utils/funciones.py
+++ src/utils/funciones.py
@@ -1,14 +1,13 @@
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+pd.set_option('display.max_columns', None)
+
 class EDA_funciones:
-
-    import pandas as pd
-    import numpy as np
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-    from matplotlib.gridspec import GridSpec
-
-    pd.set_option('display.max_columns', None)
-
-
 
     def __init__(self):
         pass 
