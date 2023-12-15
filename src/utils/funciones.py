a19977a0d3a1a0f072ab4c3287ca21908842391c
mariaortizz <ortizmariadelrosario@gmail.com>
2023-12-15 18:43:50 +0200

    ejer 15/12

--- src/utils/funciones.py
+++ src/utils/funciones.py
@@ -1,4 +1,15 @@
 class EDA_funciones:
+
+    import pandas as pd
+    import numpy as np
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    from matplotlib.gridspec import GridSpec
+
+    pd.set_option('display.max_columns', None)
+
+
+
     def __init__(self):
         pass 
 
